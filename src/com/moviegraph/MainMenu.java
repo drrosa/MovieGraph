9ae47ff283c15e89af6520d9c51dfbd79521d18b
Norman <Norman>
2013-12-02 13:02:40 -0600

    add menus to logout, and destroy sharedprefs

--- src/com/moviegraph/MainMenu.java
+++ src/com/moviegraph/MainMenu.java
@@ -2,18 +2,29 @@ package com.moviegraph;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import java.io.File;
 
 
 public class MainMenu extends Activity {
 
     //    TODO: LISTID constants
-
+    SharedPreferences prefs;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState); 
+		super.onCreate(savedInstanceState);
+        prefs = this.getSharedPreferences("myPreferences", Context.MODE_PRIVATE);
+//        String email = prefs.getString("email", null);
+//        Log.d("Session", "SharedPreferences exist, email= "+ email);
 		setContentView(R.layout.main_menu);
 	}
 
@@ -43,5 +54,41 @@ public class MainMenu extends Activity {
         intent.putExtra("listID", 2);
         startActivity(intent);
     }
+
+
+
+    // create the Activity's menu from a menu resource XML file
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu_menu, menu);
+        return true;
+    }
+
+    // handle choice from options menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+                        //The only item is Lougout so it will delete preferences
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.commit();
+
+                String email = prefs.getString("email", null);
+                Log.d("Session", "SharedPreferences exist, email= "+ email);
+
+        Intent addNewContact;
+        // create a new Intent to launch
+
+            addNewContact =
+                    new Intent(this, WelcomeScreen.class);
+
+
+        startActivity(addNewContact);
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
